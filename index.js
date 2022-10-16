49424ac7081ea6fb6ab67847286f2072b68e9c69
Sharifaaz <Sharifaalzayed96@gmail.com>
2022-10-16 10:14:24 +0300

    favourite animals complete

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+
+const express = require('express')
 const app = express()
 const PORT = 3000
+const ejsLayouts = require('express-ejs-layouts')
 
 // MIDDLEWARE
 app.set('view engine','ejs')
+app.use(ejsLayouts)
 
 // ROUTES
+
+app.get('/', (req,res)=>{
+    res.render('home.ejs')
+})
+
+// favorite animals 
+app.get('/animals', (req,res)=>{
+    const faveAnimals = ['sand crab','corny joke dog','benedict the sea cucamber']
+    res.render('animal.ejs', {animals: faveAnimals})
+})
+
 app.listen (PORT, ()=>{
-    console.log("LAYOUTS AND CONTROLLERS")
+    console.log("LAYOUTS AND CONTROLLERS"+ PORT)
 })
